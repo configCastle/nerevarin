4410f0485b43d77cacb80c54d607df33dbf80ce0
Eduard Lemmer <drdlemmer@gmail.com>
2020-06-12 03:25:39 +0300

    Add basic register user (#2)
    * Add style components
    
    * Add __pycache__ to .gitignore
    
    * Add utilites for db operation
    
    * Add base register new user

--- app.py
+++ app.py
@@ -3,9 +3,11 @@ import asyncio
 import os
 import sys
 
+import aiohttp_cors
 from aiohttp import web
 
 from constants import standart_port
+from routes import init_routes
 from utils.db import init_db
 
 
@@ -17,9 +19,22 @@ def init_app():
         app: instance of application
     """
     app = web.Application()
+
     loop = asyncio.get_event_loop()
     db = loop.run_until_complete(init_db())
     app['db'] = db
+
+    init_routes(app)
+
+    client_uri = os.getenv('CLIENT_URI', '*')
+    aiohttp_cors.setup(app, defaults={
+        client_uri: aiohttp_cors.ResourceOptions(
+            expose_headers='*',
+            allow_headers='*',
+            allow_credentials=True,
+            allow_methods=['POST', 'GET', 'OPTIONS'],
+        ),
+    })
 
     return app
 
