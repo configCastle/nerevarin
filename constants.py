eeda3ee15175ccc2ee4923c9f20e9468adc2c874
Eduard Lemmer <drdlemmer@gmail.com>
2020-06-13 22:40:16 +0300

    Add env for jwt life

--- constants.py
+++ constants.py
@@ -1,9 +1,12 @@
 """Constant for application."""
 import os
 
+standart_jwt_minutes = 30
+standart_jwt_days = 60
+
 JWT_ALGORITHM = 'HS256'
-JWT_EXP_DELTA_MINUTES = 30
-JWT_EXP_DELTA_DAYS = 60
+JWT_EXP_DELTA_MINUTES = os.getenv('LIFE_MINUTE', standart_jwt_minutes)
+JWT_EXP_DELTA_DAYS = os.getenv('LIFE_DAYS', standart_jwt_days)
 JWT_SECRET = os.getenv('JWT_SECRET', 'secret')
 
 standart_port = 8080
