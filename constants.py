e48176a82c32203e453d54235ca384617f88020c
Eduard Lemmer <drdlemmer@gmail.com>
2020-06-13 22:46:50 +0300

    Fix type for jwt life

--- constants.py
+++ constants.py
@@ -5,8 +5,8 @@ standart_jwt_minutes = 30
 standart_jwt_days = 60
 
 JWT_ALGORITHM = 'HS256'
-JWT_EXP_DELTA_MINUTES = os.getenv('LIFE_MINUTE', standart_jwt_minutes)
-JWT_EXP_DELTA_DAYS = os.getenv('LIFE_DAYS', standart_jwt_days)
+JWT_EXP_DELTA_MINUTES = int(os.getenv('LIFE_MINUTE', standart_jwt_minutes))
+JWT_EXP_DELTA_DAYS = int(os.getenv('LIFE_DAYS', standart_jwt_days))
 JWT_SECRET = os.getenv('JWT_SECRET', 'secret')
 
 standart_port = 8080
